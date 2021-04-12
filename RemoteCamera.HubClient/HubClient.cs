3392c6f375a39e6544784f77f87906a0a0ec1160
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2021-04-12 21:51:07 +0300

    new library for signalr is added

---
+++ RemoteCamera.HubClient/HubClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace RemoteCamera.HubClient
+{
+    public class HubClient
+    {
+        private readonly HubConnection _connection;
+
+        public HubClient(string url)
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://192.168.0.102:5000/hub")
+                .Build();
+        }
+
+        private async Task ConnectAsync()
+        {
+            await _connection.StartAsync();
+
+            _connection.On<string>("J", val => UIApplication.SharedApplication.InvokeOnMainThread(()
+                =>
+            {
+                var dtGot = DateTime.Now;
+                textLabel.Text += $"Sent: {sentDt}, got: {dtGot:O}{Environment.NewLine}";
+                Console.WriteLine($"Roundtrip: {(sentDt - dtGot).TotalMilliseconds}ms");
+            }));
+        }
+    }
+}
---
+++ RemoteCameraControl.HubClient/HubClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace RemoteCameraControl.HubClient
+{
+    public class HubClient
+    {
+        private object _connection;
+
+        public HubClient(string url)
+        {
+            _connection = new HubConnectionBuilder()
+            .WithUrl(url)
+            .Build();
+        }
+
+        public async Task Connect(
+        {
+
+        }
+    }
+}
